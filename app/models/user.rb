ffbd09cc1e602b2063c4b9a7c00157a5391928d3
phyirome <hirohiroxmt@gmail.com>
2020-09-04 14:50:23 +0900

    テストコードの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname, :email, :password, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday, presence: true
+
+    validates :nickname, :email, :password, :family_name, :first_name, :kana_family_name, :kana_first_name, :birthday, presence: true
+
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'は全角文字を使用してください' } do
+      validates :family_name
+      validates :first_name
+      validates :kana_family_name
+      validates :kana_first_name
+    end
+
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+
 end
