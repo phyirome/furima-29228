6fe5592267ae23b3b376d860bf494a8830426a5c
phyirome <hirohiroxmt@gmail.com>
2020-09-05 12:06:54 +0900

    userモデルにバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :condition, :shipping_day, :shipping_fee, :shipping_from
+
+  validates :category, :condition, :shipping_day, :shipping_fee, :shipping_from, presence: true
+
+  validates :name, :price, :text, :category_id, :condition_id, :shipping_day_id, :shipping_fee_id, :shipping_from_id, numericality: { other_than: 0 }
+  
 end
