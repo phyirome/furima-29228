7d8cd1628dd6337cf436601cf1dfb9de63341089
phyirome <hirohiroxmt@gmail.com>
2020-09-06 08:03:33 +0900

    rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,10 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
-  
 
   private
 
   def item_params
     params.require(:image).permit(:name, :price, :text, :image)
   end
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,12 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category, :condition, :shipping_day, :shipping_fee, :shipping_from
 
   validates :category, :condition, :shipping_day, :shipping_fee, :shipping_from, presence: true
 
   validates :name, :price, :text, :category_id, :condition_id, :shipping_day_id, :shipping_fee_id, :shipping_from_id, numericality: { other_than: 0 }
-  
 end
