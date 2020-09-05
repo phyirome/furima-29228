59f95e7cb13aa8061e20e518a6be4b4f52e9435f
phyirome <hirohiroxmt@gmail.com>
2020-09-05 15:41:14 +0900

    itemsコントローラーのindexアクションにItem.allを設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
+  
+
+  private
+
+  def item_params
+    params.require(:image).permit(:name, :price, :text, :image)
+  end
+
 end
