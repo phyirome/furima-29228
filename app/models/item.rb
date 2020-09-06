b433619094afd26e840b4cdd3069f2b894b4107a
phyirome <hirohiroxmt@gmail.com>
2020-09-06 20:49:57 +0900

    ActiveHashの記述修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,11 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :condition, :shipping_day, :shipping_fee, :shipping_from
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_day
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :shipping_from
 
   validates :category, :condition, :shipping_day, :shipping_fee, :shipping_from, presence: true
 
