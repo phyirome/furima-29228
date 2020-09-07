914b2a3681751aa18cf5e7f450fbb0ede02509dd
phyirome <hirohiroxmt@gmail.com>
2020-09-07 12:50:58 +0900

    itemsコントローラーに#new#createを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,9 +3,22 @@ class ItemsController < ApplicationController
     @items = Item.all.order("created_at DESC")
   end
 
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def item_params
-    params.require(:image).permit(:name, :price, :text, :image)
+    params.require(:image).permit(:name, :price, :text, :image, :category_id, :condition_id, :shipping_day_id, :shipping_fee_id, :shipping_from_id)
   end
 end
