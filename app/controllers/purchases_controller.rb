c85154e9f85912432e1ff635e0b8c8f89f4782d8
phyirome <hirohiroxmt@gmail.com>
2020-09-11 00:03:42 +0900

    ログインしているかつ自分が出品していないもののみ購入画面に進める

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,24 @@
 class PurchasesController < ApplicationController
+  before_action :move_to_login_page
+  before_action :move_to_top_page
+
+  def index
+    @item = Item.find(params[:item_id])
+    # @item = Purchase.item_id.find(params[:id])
+    # binding.pry
+  end
+  
+  def create
+  end
+  
+  def move_to_login_page
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+  
+  def move_to_top_page
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id
+    # @item.purchase != 0
+  end
+
 end
