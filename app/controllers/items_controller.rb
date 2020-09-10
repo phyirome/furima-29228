7ad802c624266511c2e14a785ebf544debc0bb08
phyirome <hirohiroxmt@gmail.com>
2020-09-10 12:20:20 +0900

    itemsコントローラーにdestroyアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,12 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+  
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
 
   private
 
