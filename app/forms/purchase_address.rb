c166d4ae5fd31b14485a06d9fffef074474b94c6
phyirome <hirohiroxmt@gmail.com>
2020-09-11 20:21:55 +0900

    formオブジェクトの設定

---
+++ app/forms/purchase_address.rb
@@ -0,0 +1,24 @@
+class PurchaseAddress
+
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number
+
+  validates :address, presence: {message:"が空欄です。"}
+
+  validates :prefecture_id, numericality: { other_than: 0, message:"を選択してください。" }
+
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message:"は無効です。半角数字とハイフンで入力してください。"}
+    validates :phone_number, format: { with: /\A[0-9]+\z/, message:"は無効です。半角数字で入力してください。"}
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/,message:"は無効です。全角で入力してください。"}
+    validates :item_id
+    validates :user_id
+  end
+
+  def save
+    purchase = Purchase.create(user_id: user_id, item_id: item_id)
+    # ↑.create(テーブルのカラム名1: フォームのname属性1, テーブルのカラム名2, フォームのname属性2 )
+    Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number)
+  end
+
+end
--- app/models/address.rb
+++ app/models/address.rb
@@ -4,8 +4,4 @@ class Address < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
 
-  validates :postal_code, :string, :address, :phone_number, presence: true
-
-  validates :prefecture_id, numericality: { other_than: 0 }
-  
 end
